83536fcc3750f72c8bb8f1098144427e6945313e
honkyhank82 <honky.hank82@gmail.com>
2025-11-29 02:16:17 -0500

    Auto-sync: 2025-11-29 02:16:16

--- app-web/src/screens/ManageSubscription.tsx
+++ app-web/src/screens/ManageSubscription.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { subscriptionService, SubscriptionStatus } from '../services/subscriptionService'
 import '../styles/premium.css'
